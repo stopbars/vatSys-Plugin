d0069c12bb781d6ad219f9482dd9af6da1c358fa
Charlie <84845481+llamavert@users.noreply.github.com>
2025-09-25 13:18:07 +1000

    Remove ! from BARS usage notification

--- BARS.cs
+++ BARS.cs
@@ -104,7 +104,7 @@ namespace BARS
 
             if (!hasBars)
             {
-                infoList.Add("BARS in use! - stopbars.com");
+                infoList.Add("BARS in use - stopbars.com");
             }
 
             Network.ControllerInfo = infoList.ToArray();
